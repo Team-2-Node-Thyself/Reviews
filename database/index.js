5c87e5a91e29e925eda65499c8a6c6d8724277fb
Andrew Vance <andrewaddress2@gmail.com>
2021-02-04 15:44:22 -0800

    database models and connection

--- database/index.js
+++ database/index.js
@@ -1,8 +1,48 @@
-const mysql = require('mysql2');
-const Sequelize = require('sequelize');
+const mongoose = require('mongoose');
 
-// Shinbobo88!
-module.exports = new Sequelize('bullseye_reviews', 'root', 'Shinbobo88!', {
-  host: 'localhost',
-  dialect: 'mysql'
+mongoose.connect('mongodb://localhost/sdc', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.log('error connecting to database');
 });
+
+db.once('open', () => {
+  console.log('connected to database');
+});
+
+const productSchema = new mongoose.Schema({
+  productId: Number,
+  name: String,
+  price: String,
+  description: String
+});
+
+const reviewsSchema = new mongoose.Schema({
+  productId: Number,
+  reviews: [
+    {
+      reviewId: Number,
+      title: String,
+      body: String,
+      helpful_count: Number,
+      username: String,
+      overall_rating: Number,
+      value_rating: Number,
+      quality_rating: Number,
+      would_recommend: Boolean,
+      verified_purchaser: Boolean,
+      images: [String]
+    }
+  ]
+});
+
+
+const Product = mongoose.model('product', productSchema);
+const Reviews = mongoose.model('reviews', reviewsSchema);
+
+module.exports = {
+  Product: Product,
+  Reviews: Reviews
+}
