ea50f4c16b1a93eb241146d4ef638d7c59967021
Andrew Vance <andrewaddress2@gmail.com>
2021-02-05 17:18:15 -0800

    updates routes for updating helpful_count for reviews

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -23,27 +24,32 @@ app.get('/products/:product_id/reviews', (req, res) => {
     .then(data => res.send(data));
 })
 
-app.get('/products/:review_id/images', (req, res) => {
-  Photo.findAll({
-    where: {
-      reviewId: req.params.review_id
-    }
-  }).then(data => res.send(data));
+app.patch('/products/:product_id/:review_id/helpful', (req, res) => {
+
+  Reviews.findOne({productId: req.params.product_id})
+    .then(doc => {
+      let item = doc.reviews.find(({ reviewId }) =>  reviewId === Number(req.params.review_id));
+      item.helpful_count++;
+      doc.save();
+      res.status(200).end();
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+
 })
 
-app.patch('/products/:id/helpful', (req, res) => {
-  Review.update(
-    { helpful_count: db.literal('helpful_count + 1') },
-    { where: { id: req.params.id } }
-  ).then(() => { Review.findByPk(req.params.id).then(data => res.send(data)) });
-
-})
-
-app.patch('/products/:id/not_helpful', (req, res) => {
-  Review.update(
-    { helpful_count: db.literal('helpful_count - 1') },
-    { where: { id: req.params.id } }
-  ).then(() => { Review.findByPk(req.params.id).then(data => res.send(data)) });
+app.patch('/products/:product_id/:review_id/not_helpful', (req, res) => {
+  Reviews.findOne({productId: req.params.product_id})
+    .then(doc => {
+      let item = doc.reviews.find(({ reviewId }) =>  reviewId === Number(req.params.review_id));
+      item.helpful_count--;
+      doc.save();
+      res.status(200).end();
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 
 })
 
